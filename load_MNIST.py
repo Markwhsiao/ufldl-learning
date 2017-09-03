7170fc47a676d36c598b6dc1e643fcc424222859
Markwhsiao <xiao421182@163.com>
2017-09-03 19:19:56 +0800

    Update load_MNIST.py
    used the bumpy.fromfile

--- load_MNIST.py
+++ load_MNIST.py
@@ -1,59 +1,32 @@
 import numpy as np
-import struct
-
-def reshape(mat,n,m):
-        '''
-        reshape col main 
-        '''
-        mat = np.reshape(mat,(n,m),order='F')
-        return mat
 
 def loadMniImageSet(filename):
-    """
-    return [784*m] mat
-    """
-    print "load image set",filename
-    binfile= open(filename, 'rb')
-    buffers = binfile.read()
- 
-    head = struct.unpack_from('>IIII' , buffers ,0)
-    print "head,",head
-    offset = struct.calcsize('>IIII')
-    imgNum = head[1]
-    width = head[2]
-    height = head[3]
-    bits = imgNum * width * height
-    bitsString = '>' + str(bits) + 'B' #like '>47040000B'
-    imgs = struct.unpack_from(bitsString,buffers,offset)
- 
-    binfile.close()
-    imgs = reshape(imgs,width*height,imgNum)
-    print "load imgs finished"
-    return imgs
+    f = open(filename,"r")
+    magic    = np.fromfile(f,dtype=np.dtype('>i4'),count=1)
+    imageNum = np.fromfile(f,dtype=np.dtype('>i4'),count=1)
+    width    = np.fromfile(f,dtype=np.dtype('>i4'),count=1)
+    hght     = np.fromfile(f,dtype=np.dtype('>i4'),count=1)
+    
+    images   = np.fromfile(f,dtype=np.ubyte)
+    images   = images.reshape((imageNum,width*hght)).transpose()
+    images   = images.astype(np.float64)/255
+    f.close()
+    return images
 
 def loadMniLabelSet(filename):
-    """
-    return [1*m]
-    """
-    print "load label set",filename
-    binfile = open(filename, 'rb')
-    buffers = binfile.read()
- 
-    head = struct.unpack_from('>II' , buffers ,0)
-    print "head,",head
-    imgNum=head[1]
-    offset = struct.calcsize('>II')
-    numString = '>'+str(imgNum)+"B"
-    labels = struct.unpack_from(numString , buffers , offset)
-    binfile.close()
-    labels = np.reshape(labels,(imgNum),order='F')
-    print 'load label finished'
-
+    f = open(filename,"r")
+    magic    = np.fromfile(f,dtype='>i4',count=1)
+    imageNum = np.fromfile(f,dtype='>i4',count=1)
+    labels = np.fromfile(f,dtype=np.ubyte)
+    f.close()
     return labels
 
-
 if __name__ == "__main__":
-    im     = loadMniImageSet("./mnist/t10k-images-idx3-ubyte")
-    labels = loadMniLabelSet("./mnist/train-labels-idx1-ubyte")
-    print im.shape
-    print labels.shape
+    train_image  = loadMniImageSet("./mnist/train-images-idx3-ubyte")
+    train_labels = loadMniLabelSet("./mnist/train-labels-idx1-ubyte")
+    test_image   = loadMniImageSet("./mnist/t10k-images-idx3-ubyte")
+    test_label   = loadMniLabelSet("./mnist/t10k-labels-idx1-ubyte")
+    np.savetxt("train_image.txt",train_image)
+    np.savetxt("train_labels.txt",train_labels)
+    np.savetxt("test_image.txt",test_image)
+    np.savetxt("test_label.txt",test_label)
